87492e9293a76ad776359b9e5d866e52fb3b7053
muhammadriskifauzi10 <muhammadriskifauzi10@gmail.com>
2025-01-22 19:54:49 +0700

    Belajar menampilkan data manual dari Array

--- app/Http/Controllers/Product/MainController.php
+++ app/Http/Controllers/Product/MainController.php
@@ -9,8 +9,27 @@ class MainController extends Controller
 {
     public function index()
     {
+        $data_sabun = [
+            [
+                'nama_produk' => 'Sabun Lifeboy',
+                'harga' => 5000,
+                'qty' => 5,
+            ],
+            [
+                'nama_produk' => 'Sabun Nuvo',
+                'harga' => 6000,
+                'qty' => 50,
+            ],
+            [
+                'nama_produk' => 'Indomie Goreng',
+                'harga' => 3500,
+                'qty' => 10,
+            ],
+        ];
+
         $data = [
-            'judul' => 'Produk'
+            'judul' => 'Produk',
+            'data_sabun' => $data_sabun
         ];
 
         return view('produk.main', $data);
--- resources/views/produk/main.blade.php
+++ resources/views/produk/main.blade.php
@@ -2,6 +2,31 @@
 
 @section('mycontents')
     <div>
-        Ini halaman Produk
+        <h1>Ini halaman Produk</h1>
+
+        {{-- data --}}
+        <table border="1" cellspacing="0" cellpadding="10" width="100%" style="margin-top: 20px; text-align: center">
+            <thead>
+                <tr>
+                    <th width="40">No</th>
+                    <th>Nama Produk</th>
+                    <th>Harga Produk</th>
+                    <th>Jumlah Produk</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $no = 1;
+                @endphp
+                @foreach ($data_sabun as $item)
+                    <tr>
+                        <td>{{ $no++ }}</td>
+                        <td>{{ $item['nama_produk'] }}</td>
+                        <td>RP. {{ $item['harga'] }}</td>
+                        <td>{{ $item['qty'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
