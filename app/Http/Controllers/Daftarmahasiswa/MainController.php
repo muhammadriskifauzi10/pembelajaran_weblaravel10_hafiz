e6268580896c2da9777a50ad32c5eba4b398a538
muhammadriskifauzi10 <muhammadriskifauzi10@gmail.com>
2025-01-30 20:06:07 +0700

    Belajar terhubung ke database

--- app/Http/Controllers/Daftarmahasiswa/MainController.php
+++ app/Http/Controllers/Daftarmahasiswa/MainController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Daftarmahasiswa;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
     public function index()
     {
+        $data_mahasiswa = DB::table('daftar_mahasiswa')->get();
+
         $data = [
-            'judul' => 'Daftar Mahasiswa'
+            'judul' => 'Daftar Mahasiswa',
+            'data_mahasiswa' => $data_mahasiswa
         ];
 
         return view('daftarmahasiswa.main', $data);
--- resources/views/daftarmahasiswa/main.blade.php
+++ resources/views/daftarmahasiswa/main.blade.php
@@ -2,6 +2,33 @@
 
 @section('mycontents')
     <div>
-        Ini halaman Daftar Mahasiswa
+        <h1>Ini halaman Daftar Mahasiswa</h1>
+
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama Mahasiswa</th>
+                    <th>Umur</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Jurusan</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @php
+                    $no = 1;
+                @endphp
+                @foreach ($data_mahasiswa as $row)
+                    <tr>
+                        <td>{{ $no++ }}</td>
+                        <td>{{ $row->nama_mahasiswa }}</td>
+                        <td>{{ $row->umur }}</td>
+                        <td>{{ $row->jenis_kelamin }}</td>
+                        <td>{{ $row->jurusan }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
